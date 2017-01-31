3b845d5f00bd298a015f7a3df7899b57fe5c866d
neurosystemeu <michal.feliksik@gmail.com>
2017-01-31 14:09:00 +0100

    poprawka projektu

---
+++ VirtualMachine/UnitTestVirtualMachine/Klasy/Biznesowe/ObiektBiznesowy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NeuroSystem.UnitTestVirtualMachine.Klasy.Biznesowe
+{
+    public class ObiektBiznesowy
+    {
+        public Guid Id { get; set; }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var ob = obj as ObiektBiznesowy;
+            if (ob != null)
+            {
+                return ob.Id == Id && ob.GetType() == this.GetType();
+            }
+            else
+            {
+                return base.Equals(obj);
+            }
+        }
+    }
+}
