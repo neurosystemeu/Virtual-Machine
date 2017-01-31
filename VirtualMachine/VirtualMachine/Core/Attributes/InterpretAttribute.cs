e1d7dcba2bd1a2b59e2f4cc2abf41cdc9c2fbae0
neurosystemeu <michal.feliksik@gmail.com>
2017-01-31 14:06:00 +0100

    add

---
+++ VirtualMachine/VirtualMachine/Core/Attributes/InterpretAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NeuroSystem.VirtualMachine.Core.Attributes
+{
+    /// <summary>
+    /// Atrybut oznacza że dana metoda powinna być interpretowana przez wirutalną maszynę
+    /// Metody bez tego znacznika będą wykonane
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class InterpretAttribute : System.Attribute
+    {
+    }
+}
---
+++ VirtualMachine/VirtualMachine/Core/Serialization/NsTypedParameterExpression.cs
@@ -0,0 +1,10 @@
+﻿namespace NeuroSystem.VirtualMachine.Serializacja
+{
+    public class NsTypedParameterExpression : System.Linq.Expressions.Expression
+    {
+        public NsTypedParameterExpression()
+        {
+
+        }
+    }
+}
